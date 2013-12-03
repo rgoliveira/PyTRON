262a2c4e274ce993db1c3854c800b1d1aaef7261
rgoliveira <ricardo.godoy@inf.ufrgs.br>
2013-12-03 02:56:19 -0200

    preparing model files for oponents implementation

--- src/filenames.py
+++ src/filenames.py
@@ -2,6 +2,8 @@ class Filenames:
     class models:
         base        = './res/models/player/'
         player      = base + 'bicycle.obj'
+        base        = './res/models/oponents/'
+        player      = base + 'bicycle.obj'
     class textures:
         base        = './res/textures/'
         floor_tile  = base + 'floor.png'
